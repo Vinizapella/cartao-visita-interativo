96f637a9be0c84c69ff74f3851a242ab82affd50
VINICIUS DOS SANTOS ZAPELLA <vinicius_zapella@estudante.sesisenai.org.br>
2025-10-31 19:29:31 -0300

    feat: questão 2

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ nomeInput.addEventListener('input', () => {
     cartaoNome.textContent = nomeInput.value;
 });
 
+const cartao = document.getElementById('cartao');
+const btnAzul = document.getElementById('btn-azul');
+const btnVerde = document.getElementById('btn-verde');
+
+btnAzul.addEventListener('click', () => {
+    cartao.classList.remove('fundo-verde');
+    cartao.classList.add('fundo-azul');
+});
+
+btnVerde.addEventListener('click', () => {
+    cartao.classList.remove('fundo-azul');
+    cartao.classList.add('fundo-verde');
+});
+
